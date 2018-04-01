3eb401f9b6f24471afd50461d0abd1c0ce78ed53
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 15:49:02 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where(rating: self.highest_rating)
+    Show.where(rating: self.highest_rating).take
   end
 
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where(rating: self.lowest_rating)
+    Show.where(rating: self.lowest_rating).take
   end
 
   def self.ratings_sum
